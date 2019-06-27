b2c8f8c0e6ac2a5aa9f89404b82c1d3a3a8d57cd
César Filho <=>
2019-06-26 21:39:00 -0300

    configuração do layout

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index()
     {
-        return 'aaawerwes';
+        $user =  Auth::user();
+        return view('layouts.dash.dashboard',['user'=>$user]);
     }
 }
